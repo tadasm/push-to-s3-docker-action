de8f66796ba2ccc6ebd246b1d79df635d049977b
Tadas Miniotas <tadas.miniotas@transfergo.com>
2021-10-01 13:02:07 +0300

    reworked action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-aws s3 sync --dryrun  --exclude "*.git*" . s3://resume.bofh.lt --region eu-west-1
+echo "test env var"
+echo "$AWS_ACCESS_KEY_ID"
+#aws s3 sync --dryrun  --exclude "*.git*" . s3://resume.bofh.lt --region eu-west-1
 echo $GITHUB_REPOSITORY
 echo $GITHUB_WORKSPACE
 #aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*' --profile florian.ec --region eu-west-1
